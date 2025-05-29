2d92d0d515e115d8cdcd716f2a7f4bf390983569
EsotericVerse <sopa2306@gmail.com>
2025-05-29 11:23:39 +0800

    PWA

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,23 @@
+
 self.addEventListener('install', event => {
   console.log('[SW] 安裝完成');
-  self.skipWaiting();
+  event.waitUntil(
+    caches.open('moon-runes-cache-v1').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './data/runes.json',
+        './images/41_語.png',
+        './images/42_憶.png'
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
