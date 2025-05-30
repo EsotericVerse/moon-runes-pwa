7a9d8408df5a75e02b1b92eaac4217705691ddb7
EsotericVerse <sopa2306@gmail.com>
2025-05-30 10:06:39 +0800

    PWA RWD

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/js/main.js',
+        '/images/41_語.png',
+        // 更多需要快取的檔案
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
