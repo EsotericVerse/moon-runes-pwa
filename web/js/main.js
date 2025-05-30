141fb6a6b92e832e9ef1eb9b359c81b864e3f997
EsotericVerse <sopa2306@gmail.com>
2025-05-30 08:13:40 +0800

    add another branch

--- web/js/main.js
+++ web/js/main.js
@@ -1,8 +1,7 @@
 window.addEventListener("DOMContentLoaded", () => {
   const image = document.getElementById("rune-image");
   const card = document.getElementById("rune-card");
-
   card.addEventListener("click", () => {
-    window.location.href = "result.html"; // 之後要實作的占卜畫面
+    window.location.href = "result.html";
   });
 });
