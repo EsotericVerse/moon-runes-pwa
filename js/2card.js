0fac68e90a83f065e35340f5afe49d7c725611fa
EsotericVerse <sopa2306@gmail.com>
2025-07-21 17:33:49 +0800

    Update 2card.js

--- js/2card.js
+++ js/2card.js
@@ -1,9 +1,7 @@
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [mediator: {
-      [array[i], array[j]] = [array[j], array[i]];
-    }
+    [array[i], array[j]] = [array[j], array[i]];
   }
 }
 
