127de8640cd5032bcf327c87a35219198ec9889a
EsotericVerse <sopa2306@gmail.com>
2025-07-25 15:42:14 +0800

    Create c.py

---
+++ engine/c.py
@@ -0,0 +1,9 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+# 下載並載入 tokenizer 和模型（第一次會自動下載到本地 ~/.cache/huggingface/hub）
+tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm3-6b", trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained("THUDM/chatglm3-6b", trust_remote_code=True)
+
+# 若有 NVIDIA 顯卡可加上 .half().cuda()，否則用 .float() 跑在 CPU
+# model = model.half().cuda()
+model = model.float()  # CPU 跑會比較慢
