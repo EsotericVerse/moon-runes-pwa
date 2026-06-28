9c208f5ef9c277fe3c87f40670eb89fe3cced0e8
EsotericVerse <sopa2306@gmail.com>
2026-06-28 08:29:32 +0800

    Fix service worker cache paths

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,38 @@
+const CACHE_NAME = 'moon-runes-pwa-v2';
+const ASSETS_TO_CACHE = [
+  '/',
+  'index.html',
+  'result.html',
+  'list.html',
+  'css/style.css',
+  'js/main.js',
+  'js/result.js',
+  'js/runeLibrary.js',
+  'js/list.js',
+  '64images/65_玄.png',
+  '64images/66_命.png',
+  'manifest.json',
+  'favicon.ico'
+];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('v1').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/js/main.js',
-        '/64images/65_玄.png',
-        // 更多需要快取的檔案
-      ]);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
