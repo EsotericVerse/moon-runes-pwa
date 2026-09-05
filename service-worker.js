3c2b2cc970ae1c0e94fcfeda0c079f7a1bf9a29e
EsotericVerse <sopa2306@gmail.com>
2026-09-05 17:36:56 +0800

    Cache ERA registry for unified search

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "moon-runes-pwa-v58";
+const CACHE_NAME = "moon-runes-pwa-v59";
 
 const ASSETS_TO_CACHE = [
   "/",
@@ -20,6 +20,7 @@ const ASSETS_TO_CACHE = [
   "/data/shared/LOC_NAV.json",
   "/data/shared/LOC_KM_KEYWORDS.json",
   "/data/shared/LOC_KNOWLEDGE_ASSET_REGISTRY.json",
+  "/data/shared/LOC_ERA_REGISTRY.json",
   "/data/shared/lots.json",
   "/js/main.js",
   "/js/locMoonPhase.js",
