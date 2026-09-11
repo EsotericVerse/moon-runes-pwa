56c5781bb0a37b6b72d776c1f2802b15a2ae6297
EsotericVerse <sopa2306@gmail.com>
2026-09-11 09:17:23 +0800

    chore(pwa): cache shared terminology governance

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "moon-runes-pwa-v196";
+const CACHE_NAME = "moon-runes-pwa-v197";
 
 const ASSETS_TO_CACHE = [
   "/",
@@ -18,6 +18,7 @@ const ASSETS_TO_CACHE = [
   "/css/style.css",
   "/js/loc-nav.js",
   "/js/loc-nav1.js",
+  "/js/public-terminology.js",
   "/js/home-canonical.js",
   "/js/loc-periods.js",
   "/css/loc-nav.css",
