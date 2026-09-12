b3fb0420f919ba3a9207b24cf5740cd9a6dd0351
EsotericVerse <sopa2306@gmail.com>
2026-09-12 22:57:52 +0800

    feat: initialize merged LOC router on loc page

--- js/main.js
+++ js/main.js
@@ -8,6 +8,15 @@ window.addEventListener("DOMContentLoaded", () => {
       window.location.href = "runes.html#draw";
     });
   }
+
+  const page = location.pathname.split("/").pop() || "index.html";
+  if (page === "loc.html" && !document.querySelector('script[data-loc-merged-router]')) {
+    const script = document.createElement("script");
+    script.src = "js/loc-merged-router.js";
+    script.defer = true;
+    script.dataset.locMergedRouter = "true";
+    document.body.appendChild(script);
+  }
 });
 
 if ("serviceWorker" in navigator) {
