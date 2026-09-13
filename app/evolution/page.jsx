4aaa7ac7d793e715f5ae7df1d5a0060b2b4f27b1
EsotericVerse <sopa2306@gmail.com>
2026-09-14 00:25:07 +0800

    ui: rename evolution page metadata to culture

--- app/evolution/page.jsx
+++ app/evolution/page.jsx
@@ -1,3 +1,3 @@
 import LocApp from '../loc/LocApp';
-export const metadata={title:'推演｜LOC 月典'};
+export const metadata={title:'文化｜LOC 月典'};
 export default function EvolutionPage(){return <LocApp/>;}
