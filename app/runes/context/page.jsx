aeeeac2439c9e667b469183b77a0fc0cb77d82e3
EsotericVerse <sopa2306@gmail.com>
2026-09-14 23:38:06 +0800

    feat(runes): move existing semantic graph into LunaRunes

---
+++ app/runes/context/page.jsx
@@ -0,0 +1,9 @@
+import ContextView from '../../loc/views/ContextView';
+
+export const metadata = { title: '符文語意圖｜月之符文｜LOC' };
+
+export default function RuneContextPage() {
+  return <main className="loc-next-main" data-loc-view="context">
+    <ContextView />
+  </main>;
+}
