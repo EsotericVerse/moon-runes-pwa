fc80ef8b2c38dc8b9751b7441641c562ab61f781
EsotericVerse <sopa2306@gmail.com>
2026-09-15 00:16:42 +0800

    refactor(runes): scope culture trajectory to LunaRunes data

--- app/runes/culture/page.jsx
+++ app/runes/culture/page.jsx
@@ -1,9 +1,9 @@
-import EvolutionView from '../../loc/views/EvolutionView';
+import RuneCultureView from '../RuneCultureView';
 
 export const metadata = { title: '符文軌跡（文化）｜月之符文｜LOC' };
 
 export default function RuneCulturePage() {
-  return <main className="loc-next-main" data-loc-view="evolution">
-    <EvolutionView />
+  return <main className="loc-next-main" data-loc-view="rune-culture">
+    <RuneCultureView />
   </main>;
 }
