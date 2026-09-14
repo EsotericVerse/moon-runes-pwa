b5184d5aecb99da85d6abd0144e7878409035d33
EsotericVerse <sopa2306@gmail.com>
2026-09-14 23:49:03 +0800

    feat(runes): copy existing statistics view into LunaRunes

---
+++ app/runes/stats/page.jsx
@@ -0,0 +1,3 @@
+import StaticsView from '../../loc/views/StaticsView';
+export const metadata={title:'符文統計｜月之符文'};
+export default function Page(){return <main className="loc-next-main"><StaticsView /></main>;}
