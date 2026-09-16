bcd24030d6d4313a2d029ce4cf0eb2d20eafd435
EsotericVerse <sopa2306@gmail.com>
2026-09-16 13:48:23 +0800

    chore(nav): keep route implementation out of NAV-only change

--- app/runes/statics/page.jsx
+++
@@ -1,7 +0,0 @@
-import StaticsView from '../../loc/views/StaticsView';
-
-export const metadata={title:'符文統計｜LunaRunes 月之符文'};
-
-export default function RunesStaticsPage(){
-  return <main className="loc-next-main" data-scope="lunarunes" data-loc-view="statics"><StaticsView scope="lunarunes" /></main>;
-}
