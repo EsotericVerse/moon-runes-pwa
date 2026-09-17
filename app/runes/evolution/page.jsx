5409dab34d55a6700b671a0a96b41351c6d13bc2
EsotericVerse <sopa2306@gmail.com>
2026-09-17 13:22:06 +0800

    Add LunaRunes culture route

---
+++ app/runes/evolution/page.jsx
@@ -0,0 +1,7 @@
+import LunaRunesEvolutionView from '../LunaRunesEvolutionView';
+
+export const metadata={title:'月之符文｜文化'};
+
+export default function LunaRunesEvolutionPage(){
+  return <LunaRunesEvolutionView/>;
+}
