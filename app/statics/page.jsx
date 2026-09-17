fcc076c2415692c80ff62c6457fbebf486ada1c4
EsotericVerse <sopa2306@gmail.com>
2026-09-17 13:25:49 +0800

    Dispatch statistics composition by Scope

--- app/statics/page.jsx
+++ app/statics/page.jsx
@@ -1,3 +1,3 @@
-import LocApp from '../loc/LocApp';
+import ScopedFeaturePage from '../ScopedFeaturePage';
 export const metadata={title:'統計｜LOC 月典'};
-export default function StaticsPage(){return <LocApp/>;}
+export default function StaticsPage(){return <ScopedFeaturePage feature="statics"/>;}
