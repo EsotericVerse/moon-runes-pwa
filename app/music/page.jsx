42a19781b3a84ee39ab853489788f83436f6ba55
EsotericVerse <sopa2306@gmail.com>
2026-09-18 22:35:41 +0800

    feat: add LOC music introduction page

---
+++ app/music/page.jsx
@@ -0,0 +1,3 @@
+import SystemCatalogView from '../loc/views/SystemCatalogView';
+export const metadata={title:'音樂｜LOC 月典'};
+export default function Page(){return <SystemCatalogView kind="music"/>;}
