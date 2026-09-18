0886ce6a839d6f1d9fec6acd6e97914042171376
EsotericVerse <sopa2306@gmail.com>
2026-09-18 20:08:26 +0800

    Add LunaRunes duel route

---
+++ app/duel/three/page.jsx
@@ -0,0 +1,9 @@
+import DuelDrawPage from '../DuelDrawPage';
+
+export const metadata = {
+  title: '三卡｜月之符文'
+};
+
+export default function DuelThreePage() {
+  return <DuelDrawPage mode="3card" />;
+}
