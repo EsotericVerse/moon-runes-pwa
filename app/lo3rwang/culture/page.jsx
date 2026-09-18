661324578db48dc0c28737a1cbf904e314c73476
EsotericVerse <sopa2306@gmail.com>
2026-09-18 22:28:22 +0800

    migrate: move personal culture route under lo3rwang scope

---
+++ app/lo3rwang/culture/page.jsx
@@ -0,0 +1,7 @@
+import Lo3rwangCultureView from '../../loc/views/Lo3rwangCultureView';
+
+export const metadata={title:'政德文化｜lo3rwang'};
+
+export default function Lo3rwangCulturePage(){
+  return <main className="loc-next-main" data-loc-view="lo3rwang-culture"><Lo3rwangCultureView/></main>;
+}
