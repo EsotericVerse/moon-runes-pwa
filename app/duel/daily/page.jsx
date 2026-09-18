b1ef8b8792af1f0eb4d425b6b8f998008f848731
EsotericVerse <sopa2306@gmail.com>
2026-09-18 20:08:21 +0800

    Add LunaRunes duel route

---
+++ app/duel/daily/page.jsx
@@ -0,0 +1,9 @@
+import DuelDrawPage from '../DuelDrawPage';
+
+export const metadata = {
+  title: '每日符文｜月之符文'
+};
+
+export default function DuelDailyPage() {
+  return <DuelDrawPage mode="daily" />;
+}
