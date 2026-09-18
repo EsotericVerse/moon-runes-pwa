000dec4aa595060926b3884193d7269207e10908
EsotericVerse <sopa2306@gmail.com>
2026-09-18 22:52:07 +0800

    feat: make algorithm page derive from Rune Grammar

--- app/algorithm/page.jsx
+++ app/algorithm/page.jsx
@@ -1,3 +1,3 @@
-import SystemCatalogView from '../loc/views/SystemCatalogView';
-export const metadata={title:'演算法｜LOC 月典'};
-export default function Page(){return <SystemCatalogView kind="algorithm"/>;}
+import RuneAlgorithmView from '../loc/views/RuneAlgorithmView';
+export const metadata={title:'符文演算法｜LOC 月典'};
+export default function AlgorithmPage(){return <RuneAlgorithmView/>;}
