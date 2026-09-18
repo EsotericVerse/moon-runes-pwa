a7c1be6d27a11aa5aabf90621c567884efaec4ab
EsotericVerse <sopa2306@gmail.com>
2026-09-18 20:08:23 +0800

    Add LunaRunes duel route

---
+++ app/duel/two/page.jsx
@@ -0,0 +1,9 @@
+import DuelDrawPage from '../DuelDrawPage';
+
+export const metadata = {
+  title: '雙卡｜月之符文'
+};
+
+export default function DuelTwoPage() {
+  return <DuelDrawPage mode="2card" />;
+}
