1892ef51486671a4dd911c8e5216cdac0813cfef
EsotericVerse <sopa2306@gmail.com>
2026-09-19 05:38:04 +0800

    Add lrunes directory Scope route shell

---
+++ app/lrunes/page.jsx
@@ -0,0 +1,10 @@
+import LocApp from '../loc/LocApp';
+
+export const metadata={
+  title:'月之符文｜LOC',
+  description:'LunaRunes directory ingress under LOC.'
+};
+
+export default function LunaRunesScopePage(){
+  return <LocApp/>;
+}
