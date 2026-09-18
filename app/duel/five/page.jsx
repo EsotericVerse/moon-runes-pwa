d7253cca3271ecf3d466340036fc3702ffed7878
EsotericVerse <sopa2306@gmail.com>
2026-09-18 20:08:31 +0800

    Add LunaRunes duel route

---
+++ app/duel/five/page.jsx
@@ -0,0 +1,9 @@
+import DuelDrawPage from '../DuelDrawPage';
+
+export const metadata = {
+  title: '五卡｜月之符文'
+};
+
+export default function DuelFivePage() {
+  return <DuelDrawPage mode="5card" />;
+}
