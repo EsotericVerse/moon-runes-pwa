9f86cb43c2ca1603797c6d5547e213d4d9ef4439
EsotericVerse <sopa2306@gmail.com>
2026-09-19 05:54:00 +0800

    Extract route-independent LunaRunes history page

---
+++ app/runes/RuneHistoryPage.jsx
@@ -0,0 +1,7 @@
+import HistoryClient from './history/HistoryClient';
+
+export const metadata={title:'抽籤紀錄｜月之符文'};
+
+export default function RuneHistoryPage(){
+  return <main className="loc-next-main"><HistoryClient/></main>;
+}
