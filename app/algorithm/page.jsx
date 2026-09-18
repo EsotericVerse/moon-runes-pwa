21adfb62e4e00e62f0e07ec9a63aeb43de992e3c
EsotericVerse <sopa2306@gmail.com>
2026-09-18 22:35:48 +0800

    feat: add LOC algorithm introduction page

---
+++ app/algorithm/page.jsx
@@ -0,0 +1,3 @@
+import SystemCatalogView from '../loc/views/SystemCatalogView';
+export const metadata={title:'演算法｜LOC 月典'};
+export default function Page(){return <SystemCatalogView kind="algorithm"/>;}
