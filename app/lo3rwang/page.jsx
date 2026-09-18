28867a629e36a39fcd69f0ebf78c6c0255c08fb3
EsotericVerse <sopa2306@gmail.com>
2026-09-19 05:30:48 +0800

    Add directory Scope route shell

---
+++ app/lo3rwang/page.jsx
@@ -0,0 +1,10 @@
+import LocApp from '../loc/LocApp';
+
+export const metadata={
+  title:'政德｜LOC 月典',
+  description:'lo3rwang directory Scope mounted under LOC.'
+};
+
+export default function Lo3rwangScopePage(){
+  return <LocApp/>;
+}
