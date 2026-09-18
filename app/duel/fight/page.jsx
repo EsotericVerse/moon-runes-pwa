03c53ec339200ff12c8b9c4464f75587aeca5df1
EsotericVerse <sopa2306@gmail.com>
2026-09-18 22:48:59 +0800

    feat: add LunaRunes context fight route

---
+++ app/duel/fight/page.jsx
@@ -0,0 +1,7 @@
+import GameView from '../../loc/views/GameView';
+
+export const metadata={title:'脈絡對戰｜月之符文'};
+
+export default function DuelFightPage(){
+  return <main className="loc-next-main"><GameView/></main>;
+}
