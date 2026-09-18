00f77123db936ae743291eee21776916be599759
EsotericVerse <sopa2306@gmail.com>
2026-09-18 20:08:36 +0800

    Add LunaRunes duel route

---
+++ app/duel/ow3gs/page.jsx
@@ -0,0 +1,9 @@
+import DuelDrawPage from '../DuelDrawPage';
+
+export const metadata = {
+  title: 'OW3gs｜月之符文'
+};
+
+export default function DuelOw3gsPage() {
+  return <DuelDrawPage mode="ow3gs" />;
+}
