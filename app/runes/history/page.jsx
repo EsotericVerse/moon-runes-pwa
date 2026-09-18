4ad23c4d47bd599c50cd3be6aeb783be1f105579
EsotericVerse <sopa2306@gmail.com>
2026-09-19 05:55:19 +0800

    Retire leaked public /runes route

--- app/runes/history/page.jsx
+++
@@ -1,7 +0,0 @@
-import HistoryClient from './HistoryClient';
-
-export const metadata={title:'抽籤紀錄｜月之符文'};
-
-export default function RuneHistoryPage(){
-  return <main className="loc-next-main"><HistoryClient/></main>;
-}
