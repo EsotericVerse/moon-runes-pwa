eae0ee76448df94a50cfe0537759c24e0088c356
EsotericVerse <sopa2306@gmail.com>
2026-09-19 09:16:16 +0800

    Add governance-scoped management home

---
+++ app/governance/manage/page.jsx
@@ -0,0 +1,7 @@
+import LocApp from '../../loc/LocApp';
+
+export const metadata={title:'管理者首頁｜治理｜LOC 月典'};
+
+export default function GovernanceManagePage(){
+  return <LocApp forcedView="manage"/>;
+}
