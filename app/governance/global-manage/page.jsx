f36269f0d9b1566cc89ad65c8609675af0d45f18
EsotericVerse <sopa2306@gmail.com>
2026-09-19 09:16:17 +0800

    Add LOC global governance management home

---
+++ app/governance/global-manage/page.jsx
@@ -0,0 +1,7 @@
+import LocApp from '../../loc/LocApp';
+
+export const metadata={title:'全域管理者｜治理｜LOC 月典'};
+
+export default function GlobalGovernanceManagePage(){
+  return <LocApp forcedView="global-manage"/>;
+}
