9d6d778218796e76174db46524019ad74aeaa834
EsotericVerse <sopa2306@gmail.com>
2026-09-21 05:50:58 +0800

    fix: await Next 16 dynamic route params

--- app/statics/[section]/page.jsx
+++ app/statics/[section]/page.jsx
@@ -1,3 +1,3 @@
 import LocApp from '../../loc/LocApp';
 export function generateStaticParams(){return [{section:'keyword'},{section:'source'},{section:'import'},{section:'total'}];}
-export default function StaticsSectionPage({params}){return <LocApp forcedView="statics" forcedSection={params.section}/>;}
+export default async function StaticsSectionPage({params}){const {section}=await params;return <LocApp forcedView="statics" forcedSection={section}/>;}
