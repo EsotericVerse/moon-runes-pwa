e8b62220be00f2d0599b34b406843b039683e9b8
EsotericVerse <sopa2306@gmail.com>
2026-09-20 11:53:21 +0800

    fix: label manager NAV target as 管理者頁面

--- app/modular-v2/scope-registry.v2.js
+++ app/modular-v2/scope-registry.v2.js
@@ -77,7 +77,7 @@ export const SCOPES_V2=Object.freeze({
     routePatterns:Object.freeze([]),
     compatibilityRoutes:Object.freeze([]),
     primary:Object.freeze({label:'語彙',href:'https://lrunes.lo3rwang.cc/list/'}),
-    role:Object.freeze({label:'管理者介紹',href:'https://loc.lo3rwang.cc/lo3rwang/'}),
+    role:Object.freeze({label:'管理者頁面',href:'https://loc.lo3rwang.cc/lo3rwang/'}),
     homes:Object.freeze([
       {label:'回月之符文首頁',href:'https://lrunes.lo3rwang.cc/'},
       {label:'回月典首頁',href:'https://loc.lo3rwang.cc/'}
