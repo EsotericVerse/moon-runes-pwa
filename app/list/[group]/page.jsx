26065a24d12aa4be7ba32cc690789c4f864abab9
EsotericVerse <sopa2306@gmail.com>
2026-09-20 12:07:25 +0800

    restore: add rune group routes

---
+++ app/list/[group]/page.jsx
@@ -0,0 +1,14 @@
+import {RuneGroupPage} from '../../runes/RuneDirectoryPages';
+import {groupParams} from '../../runes/rune-directory.mjs';
+
+export function generateStaticParams(){return groupParams();}
+
+export async function generateMetadata({params}){
+  const {group}=await params;
+  return {title:`符文群組 ${group}｜月之符文`};
+}
+
+export default async function Page({params}){
+  const {group}=await params;
+  return <RuneGroupPage groupId={group}/>;
+}
