83e9f438203c717124d589a6d2ac4eeff48b529e
EsotericVerse <sopa2306@gmail.com>
2026-09-21 05:54:11 +0800

    fix: await Next 16 dynamic route params (#222)
    Fix all dynamic LOC routes for Next 16 static export by awaiting params.

--- app/lo3rwang/[section]/page.jsx
+++ app/lo3rwang/[section]/page.jsx
@@ -1,3 +1,3 @@
 import LocApp from '../../loc/LocApp';
 export function generateStaticParams(){return [{section:'style'},{section:'work'},{section:'design'},{section:'galaxy'},{section:'others'},{section:'email'}];}
-export default function AuthorSectionPage({params}){return <LocApp forcedView="home" forcedSection={params.section}/>;}
+export default async function AuthorSectionPage({params}){const {section}=await params;return <LocApp forcedView="home" forcedSection={section}/>;}
