2679571cd135247493a1c9c2c6c5419a1ab6fa42
EsotericVerse <sopa2306@gmail.com>
2026-09-20 16:03:00 +0800

    fix: register legacy author route in scope registry

--- app/modular-v2/scope-registry.v2.js
+++ app/modular-v2/scope-registry.v2.js
@@ -94,7 +94,7 @@ export const SCOPES_V2=Object.freeze({
     domain:'dlwang.lo3rwang.cc',
     aliasName:'dlwang',
     label:'作者簡介',
-    localRoutes:Object.freeze([]),
+    localRoutes:Object.freeze(['old']),
     routePatterns:Object.freeze([]),
     compatibilityRoutes:Object.freeze([]),
     mount:Object.freeze({host:'loc.lo3rwang.cc',path:'/lo3rwang'}),
