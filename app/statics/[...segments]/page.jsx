7de496dbf575322379dcdce15050a7ec60d31c47
EsotericVerse <sopa2306@gmail.com>
2026-09-21 11:07:19 +0800

    fix: enumerate static feature subroutes

--- app/statics/[...segments]/page.jsx
+++ app/statics/[...segments]/page.jsx
@@ -1,5 +1,7 @@
 import LocApp from '../../loc/LocApp';
 
+export function generateStaticParams(){return [{"segments":["keyword"]},{"segments":["units"]},{"segments":["sources"]}];}
+
 export default function StaticsSubroutePage({params}){
   const section=params?.segments?.join('/')||null;
   return <LocApp forcedView="statics" forcedSection={section}/>;
