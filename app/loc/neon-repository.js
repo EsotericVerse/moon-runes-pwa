b0e502a93c7dd01d05b0e0aff76cd84156b97e05
EsotericVerse <sopa2306@gmail.com>
2026-09-23 02:15:55 +0800

    chore: register Neon canonical rune tables in shared repository

--- app/loc/neon-repository.js
+++ app/loc/neon-repository.js
@@ -9,12 +9,12 @@ const TableSchema=z.enum([
   'api.scope_access_grants','api.scope_relations','api.scope_relation_requests','api.site_theme_styles',
   'api.loc_context_entries','api.runes_context_entries','api.lo3rwang_context_entries',
   'api.lo3rwang_period_context_entries','api.loc_rankings','api.runes_rankings',
-  'api.lo3rwang_rankings','api.lrunes_runes','api.lrunes_evolution_history',
+  'api.lo3rwang_rankings','api.lrunes_runes','api.lrunes_evolution_history','api.lrunes_harmony','api.lrunes_algorithm',
   'api.loc_works','api.runes_works','api.lo3rwang_works',
   'silver.works','silver.song_versions','silver.work_scope_affiliations',
   'silver.runes_context_entries','silver.lo3rwang_context_entries',
   'silver.lo3rwang_period_context_entries','silver.runes_rankings',
-  'silver.lo3rwang_rankings','silver.lrunes_runes','silver.lrunes_evolution_history',
+  'silver.lo3rwang_rankings','silver.lrunes_runes','silver.lrunes_evolution_history','silver.lrunes_harmony','silver.lrunes_algorithm',
   'silver.lo3rwang_works','silver.runes_works',
   'vault.work_texts','vault.song_version_texts'
 ]);
