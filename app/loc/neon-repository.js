5dc9650d9314e84e1b1aef8fd48e939e8f766915
EsotericVerse <sopa2306@gmail.com>
2026-09-23 05:14:05 +0800

    fix: restore direct Neon client data access

--- app/loc/neon-repository.js
+++ app/loc/neon-repository.js
@@ -6,7 +6,7 @@ import {neonClient} from './neon-client';
 const TableSchema=z.enum([
   'api.user_records','api.user_settings','api.scope_contacts','api.scope_theme_defaults',
   'api.scope_access_grants','api.scope_relations','api.scope_relation_requests','api.site_theme_styles',
-  'silver.works','silver.song_versions','silver.knowledge_assets','silver.content_relations','silver.work_scope_affiliations',
+  'silver.works','silver.song_versions','silver.work_semantics','silver.faq_entries','silver.knowledge_assets','silver.content_relations','silver.work_scope_affiliations',
   'silver.runes_context_entries','silver.lo3rwang_context_entries',
   'silver.lo3rwang_period_context_entries','silver.lrunes_runes','silver.lrunes_evolution_history','silver.lrunes_harmony','silver.lrunes_algorithm',
   'silver.lo3rwang_works','silver.runes_works',
