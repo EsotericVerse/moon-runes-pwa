4921e5ac02afa893a37d200511681cf4b22b00b3
EsotericVerse <sopa2306@gmail.com>
2026-09-23 02:59:33 +0800

    security: remove remaining projection tables from read allowlist

--- app/loc/neon-repository.js
+++ app/loc/neon-repository.js
@@ -6,8 +6,6 @@ import {neonClient} from './neon-client';
 const TableSchema=z.enum([
   'api.user_records','api.user_settings','api.scope_contacts','api.scope_theme_defaults',
   'api.scope_access_grants','api.scope_relations','api.scope_relation_requests','api.site_theme_styles',
-  'api.lo3rwang_period_context_entries','api.loc_rankings','api.runes_rankings',
-  'api.lo3rwang_rankings','api.lrunes_runes','api.lrunes_evolution_history','api.lrunes_harmony','api.lrunes_algorithm',
   'silver.works','silver.song_versions','silver.knowledge_assets','silver.content_relations','silver.work_scope_affiliations',
   'silver.runes_context_entries','silver.lo3rwang_context_entries',
   'silver.lo3rwang_period_context_entries','silver.runes_rankings',
