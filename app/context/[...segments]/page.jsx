4d319ca7042359ff7c9785f7a88ff90e65ca0f74
EsotericVerse <sopa2306@gmail.com>
2026-09-23 14:58:39 +0800

    chore(rc6): remove retired feature subroutes

--- app/context/[...segments]/page.jsx
+++
@@ -1,8 +0,0 @@
-import LocApp from '../../loc/LocApp';
-
-export function generateStaticParams(){return [{"segments":["keywords"]}];}
-
-export default function ContextSubroutePage({params}){
-  const section=params?.segments?.join('/')||null;
-  return <LocApp forcedView="context" forcedSection={section}/>;
-}
