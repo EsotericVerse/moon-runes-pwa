24064239e206b061ec86262ff15c96576a7284aa
EsotericVerse <sopa2306@gmail.com>
2026-09-23 14:58:50 +0800

    chore(rc6): remove retired feature subroutes

--- app/statics/[section]/page.jsx
+++
@@ -1,3 +0,0 @@
-import LocApp from '../../loc/LocApp';
-export function generateStaticParams(){return [{section:'keyword'},{section:'source'},{section:'import'},{section:'total'}];}
-export default async function StaticsSectionPage({params}){const {section}=await params;return <LocApp forcedView="statics" forcedSection={section}/>;}
