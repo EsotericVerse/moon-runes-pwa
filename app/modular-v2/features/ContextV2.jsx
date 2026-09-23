2491d8bc551d8250cf924e1ca1bd336f599fde4f
EsotericVerse <sopa2306@gmail.com>
2026-09-23 12:57:46 +0800

    simplify: render Graph and timeline only

--- app/modular-v2/features/ContextV2.jsx
+++ app/modular-v2/features/ContextV2.jsx
@@ -1,14 +1,9 @@
 'use client';
 
-import FeaturePageV2 from '../FeaturePageV2';
 import {useScopeRuntimeV2} from '../use-scope-runtime.v2';
-import {scopeDataViewV2} from '../scope-registry.v2';
 import ContextWorkbenchV2 from './ContextWorkbenchV2';
 
 export default function ContextV2(){
   const {scopeId}=useScopeRuntimeV2();
-  const view=scopeDataViewV2(scopeId,'context');
-  return <FeaturePageV2 featureId="context" subtitle="人事物的分析關聯表達">
-    <ContextWorkbenchV2 scopeId={scopeId} view={view}/>
-  </FeaturePageV2>;
+  return <ContextWorkbenchV2 scopeId={scopeId}/>;
 }
