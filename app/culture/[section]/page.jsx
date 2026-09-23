427b45f9b723835cacc91daa322a29668412666f
EsotericVerse <sopa2306@gmail.com>
2026-09-23 14:58:42 +0800

    chore(rc6): remove retired feature subroutes

--- app/culture/[section]/page.jsx
+++
@@ -1,3 +0,0 @@
-import LocApp from '../../loc/LocApp';
-export function generateStaticParams(){return [{section:'trajectory'},{section:'history'},{section:'galaxy'}];}
-export default async function CultureSectionPage({params}){const {section}=await params;return <LocApp forcedView="culture" forcedSection={section}/>;}
