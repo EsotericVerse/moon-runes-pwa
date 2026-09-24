48fdd97616a01297794570bc6bf164b3a26e47bc
EsotericVerse <sopa2306@gmail.com>
2026-09-24 11:45:10 +0800

    Remove legacy route shell

--- app/governance/[...segments]/page.jsx
+++
@@ -1,8 +0,0 @@
-import LocApp from '../../loc/LocApp';
-
-export function generateStaticParams(){return [{"segments":["law"]},{"segments":["faq"]},{"segments":["manage"]}];}
-
-export default function GovernanceSubroutePage({params}){
-  const section=params?.segments?.join('/')||null;
-  return <LocApp forcedView="governance" forcedSection={section}/>;
-}
