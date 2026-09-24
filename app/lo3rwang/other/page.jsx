2cf8ecf5b14e5d815baf040f87a56dad8527b969
EsotericVerse <sopa2306@gmail.com>
2026-09-24 17:31:41 +0800

    Restore author other page route

---
+++ app/lo3rwang/other/page.jsx
@@ -0,0 +1,7 @@
+import AuthorHomeView from '../../../loc/views/AuthorHomeView';
+
+export const metadata={title:'其他介紹｜政德｜LOC 月典'};
+
+export default function AuthorOtherPage(){
+  return <AuthorHomeView section="others"/>;
+}
