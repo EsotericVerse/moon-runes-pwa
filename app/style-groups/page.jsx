b074a4064ac172a42c241632aa8519d8cbf5f753
EsotericVerse <sopa2306@gmail.com>
2026-09-24 11:43:55 +0800

    Remove non-Current public route

--- app/style-groups/page.jsx
+++
@@ -1,8 +0,0 @@
-import LocApp from '../loc/LocApp';
-
-export const metadata={
-  title:'群組設定｜LOC 月典',
-  description:'本機個人化語言風格群組設定。'
-};
-
-export default function StyleGroupsPage(){return <LocApp/>;}
