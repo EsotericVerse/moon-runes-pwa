f0768a4272d9687b161d5e8d6ca9431ec6b81fc7
EsotericVerse <sopa2306@gmail.com>
2026-09-24 11:42:37 +0800

    Remove legacy management route

--- app/governance/global-manage/page.jsx
+++
@@ -1,7 +0,0 @@
-import ScopeManagementV2 from '../../modular-v2/ScopeManagementV2';
-
-export const metadata={title:'全域管理者｜治理｜LOC 月典'};
-
-export default function GlobalGovernanceManagePage(){
-  return <ScopeManagementV2/>;
-}
