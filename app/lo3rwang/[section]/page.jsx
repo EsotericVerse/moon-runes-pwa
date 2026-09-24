918fbd306c7d6f2977410da3f89237a5452e82a2
EsotericVerse <sopa2306@gmail.com>
2026-09-24 11:45:12 +0800

    Remove legacy route shell

--- app/lo3rwang/[section]/page.jsx
+++
@@ -1,3 +0,0 @@
-import LocApp from '../../loc/LocApp';
-export function generateStaticParams(){return [{section:'style'},{section:'work'},{section:'design'},{section:'galaxy'},{section:'others'},{section:'email'}];}
-export default async function AuthorSectionPage({params}){const {section}=await params;return <LocApp forcedView="home" forcedSection={section} forcedScope="lo3rwang"/>;}
