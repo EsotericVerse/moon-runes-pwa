c12491f5691d861a8dae8ba2133922f848cd7c2c
EsotericVerse <sopa2306@gmail.com>
2026-09-24 17:31:40 +0800

    Restore author work page route

---
+++ app/lo3rwang/work/page.jsx
@@ -0,0 +1,7 @@
+import AuthorHomeView from '../../../loc/views/AuthorHomeView';
+
+export const metadata={title:'工作與服務｜政德｜LOC 月典'};
+
+export default function AuthorWorkPage(){
+  return <AuthorHomeView section="work"/>;
+}
