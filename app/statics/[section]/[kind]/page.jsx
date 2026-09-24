369f30449796a5d2c0fec6a8678962ae7a0094b8
EsotericVerse <sopa2306@gmail.com>
2026-09-24 11:45:14 +0800

    Remove legacy route shell

--- app/statics/[section]/[kind]/page.jsx
+++
@@ -1,3 +0,0 @@
-import LocApp from '../../../loc/LocApp';
-export function generateStaticParams(){return [{section:'keyword',kind:'total'},{section:'music',kind:'total'},{section:'source',kind:'total'}];}
-export default async function StaticsSectionKindPage({params}){const {section,kind}=await params;return <LocApp forcedView="statics" forcedSection={`${section}/${kind}`}/>;}
