146483f674f7adb775f4a7f27f67dd81115e026b
EsotericVerse <sopa2306@gmail.com>
2026-09-24 11:43:57 +0800

    Remove non-Current public route

--- app/my-style/page.jsx
+++
@@ -1,8 +0,0 @@
-import SettingsHubView from '../loc/views/SettingsHubView';
-
-export const metadata={
-  title:'設定｜LOC 月典',
-  description:'顯示、個人化、時期與資料操作設定。'
-};
-
-export default function SettingsPage(){return <main className="loc-next-main"><SettingsHubView/></main>;}
