9a0363d0306e54d9d8c1ac9333612c6ce9f71bb0
EsotericVerse <sopa2306@gmail.com>
2026-09-24 11:34:58 +0800

    Remove retired writing route from scope config

--- app/modular-v2/scope-registry.v2.js
+++ app/modular-v2/scope-registry.v2.js
@@ -42,12 +42,11 @@ export const SCOPES_V2=Object.freeze({
       'style-groups',
       'style',
       'classify',
-      'writing',
       'governance/history',
       'governance/manage',
       'governance/global-manage'
     ]),
-    routePatterns:Object.freeze(['writing/:workId','governance/:section','lo3rwang/:section']),
+    routePatterns:Object.freeze(['governance/:section','lo3rwang/:section']),
     compatibilityRoutes:Object.freeze(['management']),
     primary:Object.freeze({label:'月之符文',href:'https://lrunes.lo3rwang.cc/'}),
     role:Object.freeze({label:'作者介紹',href:'https://loc.lo3rwang.cc/lo3rwang/'}),
