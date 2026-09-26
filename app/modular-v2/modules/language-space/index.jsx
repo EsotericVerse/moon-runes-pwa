1021d53b8ef9c62505228cd703741c87b85f4e47
EsotericVerse <sopa2306@gmail.com>
2026-09-26 14:56:44 +0800

    Expose shared 4D language module contract

--- app/modular-v2/modules/language-space/index.jsx
+++ app/modular-v2/modules/language-space/index.jsx
@@ -1,6 +1,7 @@
 'use client';
 
 import LanguageSpaceModule from './LanguageSpaceModule';
+export {LANGUAGE_4D_AXES,LANGUAGE_4D_FACES,language4DFace,projectLanguage4D} from './language-4d-core';
 
 export function TimeRiverSpace(props){return <LanguageSpaceModule {...props} initialFace="time"/>}
 export function SpatialAnalysisSpace(props){return <LanguageSpaceModule {...props} initialFace="space"/>}
