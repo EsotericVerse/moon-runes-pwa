468521de08cebe338d6b79900193a4b8f7006399
EsotericVerse <sopa2306@gmail.com>
2026-09-26 09:56:23 +0800

    Route theme songs to whole works

---
+++ app/lo3rwang/literature/page.jsx
@@ -0,0 +1,7 @@
+import LiteratureWorkClient from './LiteratureWorkClient';
+
+export const metadata={title:'文學作品｜政德｜LOC 月典'};
+
+export default function LiteratureWorkPage(){
+  return <LiteratureWorkClient/>;
+}
