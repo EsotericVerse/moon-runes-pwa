c42ca49ae3984eb1671de5a069556b84d8ada31a
EsotericVerse <sopa2306@gmail.com>
2026-09-26 15:04:59 +0800

    Use canonical manage table and current RPCs

--- app/loc/neon-repository.js
+++ app/loc/neon-repository.js
@@ -7,7 +7,7 @@ const TableSchema=z.enum([
   'api.user_records','api.user_settings','api.site_theme_styles',
   'api.loc_context_entries','api.loc_rankings','api.loc_timeline_entries','api.lo3rwang_work_relations_public',
   'api.runes_context_entries','api.runes_rankings','api.lo3rwang_context_entries','api.lo3rwang_rankings','api.lo3rwang_galaxy','api.lo3rwang_style_rankings',
-  'silver.manage','silver.manage','silver.loc_scope','silver.resource_visibility','silver.lo3rwang_work_relations',
+  'silver.manage','silver.resource_visibility','silver.lo3rwang_work_relations',
   'silver.lo3rwang','silver.lo3rwang_style','silver.lo3rwang_style_keywords','silver.lo3rwang_style_time',
   'silver.lo3rwang_galaxy','silver.lo3rwang_galaxy_media',
   'silver.lrunes','silver.lrunes_style_context','silver.lrunes_style_time','silver.lrunes_galaxy',
@@ -16,7 +16,7 @@ const TableSchema=z.enum([
 ]);
 const WritableTableSchema=z.enum([
   'api.user_records','api.user_settings','api.site_theme_styles',
-  'silver.loc_scope','silver.resource_visibility','silver.lo3rwang_work_relations',
+  'silver.manage','silver.resource_visibility','silver.lo3rwang_work_relations',
   'silver.lo3rwang','silver.lo3rwang_style','silver.lo3rwang_style_keywords','silver.lo3rwang_style_time',
   'silver.lo3rwang_galaxy','silver.lo3rwang_galaxy_media',
   'silver.lrunes_style_context','silver.lrunes_style_time','silver.lrunes_galaxy'
@@ -133,7 +133,7 @@ export async function deleteNeonRows(table,{filters,returning='*'}={}){
   return parseRows(result.data,table);
 }
 
-const RpcSchema=z.enum(['grant_scope_access','revoke_scope_access','request_scope_relation','decide_scope_relation_request','loc_culture_weekly_source_counts','update_lrune_keywords','lo3rwang_period_work_source_counts','lo3rwang_period_media_count','lo3rwang_period_media_metadata_page']);
+const RpcSchema=z.enum(['loc_culture_weekly_source_counts','update_lrune_keywords','lo3rwang_period_work_source_counts','lo3rwang_period_media_count','lo3rwang_period_media_metadata_page']);
 export async function callNeonRpc(name,args){
   const rpc=RpcSchema.parse(name);
   const result=await neonClient.rpc(rpc,z.record(z.string(),z.unknown()).parse(args||{}));
